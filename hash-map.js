c7bbfb60dc46075b045ee552f44b6f934e9f6ae8
Chris Gantz <chrisgantz836@gmail.com>
2018-10-08 09:18:47 -0700

    created hashmap for lor

--- hash-map.js
+++ hash-map.js
@@ -1,7 +1,7 @@
 'use strict';
 
 class HashMap {
-  constructor(initialCapacity=8) {
+  constructor(initialCapacity = 8) {
     this.length = 0;
     this._slots = [];
     this._capacity = initialCapacity;
@@ -46,7 +46,7 @@ class HashMap {
     const hash = HashMap._hashString(key);
     const start = hash % this._capacity;
 
-    for (let i=start; i<start + this._capacity; i++) {
+    for (let i = start; i < start + this._capacity; i++) {
       const index = i % this._capacity;
       const slot = this._slots[index];
       if (slot === undefined || (slot.key === key && !slot.deleted)) {
@@ -72,7 +72,7 @@ class HashMap {
 
   static _hashString(string) {
     let hash = 5381;
-    for (let i=0; i<string.length; i++) {
+    for (let i = 0; i < string.length; i++) {
       hash = (hash << 5) + hash + string.charCodeAt(i);
       hash = hash & hash;
     }
@@ -82,3 +82,5 @@ class HashMap {
 
 HashMap.MAX_LOAD_RATIO = 0.9;
 HashMap.SIZE_RATIO = 3;
+
+module.exports = HashMap;
---
+++ main.js
@@ -0,0 +1,23 @@
+'use strict';
+const hashMapClass = require('./hash-map');
+
+function main() {
+  const lor = new hashMapClass();
+
+  lor.set('Hobbit', 'Bilbo');
+  lor.set('Hobbit', 'Frodo');
+  lor.set('Wizard', 'Gandolf');
+  lor.set('Human', 'Aragon');
+  lor.set('Elf', 'Legolas');
+  lor.set('Maiar', 'The Necromancer');
+  lor.set('Maiar', 'Sauron');
+  lor.set('RingBearer', 'Gollum');
+  lor.set('LadyOfLight', 'Galadriel');
+  lor.set('HalfElven', 'Arwen');
+  lor.set('Ent', 'Treebeard');
+
+  console.log(lor.get('Maiar'));
+
+}
+
+main();
